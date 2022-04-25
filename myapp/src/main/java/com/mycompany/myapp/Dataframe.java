e98dee039fb0395d0dd066a8ff02a33a7d93a4b8
zak <zikoz5013@gmail.com>
2022-04-25 03:09:39 +0200

    contractors written and tested

--- myapp/src/main/java/com/mycompany/myapp/Dataframe.java
+++ myapp/src/main/java/com/mycompany/myapp/Dataframe.java
@@ -4,6 +4,10 @@ package com.mycompany.myapp;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -16,29 +20,17 @@ public class Dataframe {
         return dataframe;
     }
 
-    public void setDataframe(Object[][] dataframe) {
-        this.dataframe = dataframe;
-    }
-
     public ArrayList<String> getColonne_name() {
         return colonne_name;
-    }
-
-    public void setColonne_name(ArrayList<String> colonne_name) {
-        this.colonne_name = colonne_name;
     }
 
     public ArrayList<String> getColonne_type() {
         return colonne_type;
     }
 
-    public void setColonne_type(ArrayList<String> colonne_type) {
-        this.colonne_type = colonne_type;
-    }
-
-
     public Dataframe(ArrayList<String> colonne_name, ArrayList<Object> elements) {
         this.colonne_name = colonne_name ;
+        this.colonne_type = new ArrayList<String>() ;
         int n_c = colonne_name.size() ;
         int nb_t = elements.size() ;
         int n_l = nb_t/n_c ;
@@ -46,16 +38,22 @@ public class Dataframe {
 
         for (int i = 0; i < n_l; i++) {
             for (int j = 0; j < n_c; j++) {
-                dataframe[i][j] = elements.get(j + n_l*i) ; 
+                dataframe[i][j] = elements.get(i + n_l*j) ; 
             }
             colonne_type.add( elements.get(n_c*i).getClass().getName() );
         
         }
     }
 
-    public Dataframe(String cvsFilePath){
-        File file = new File(cvsFilePath);
+    public Dataframe(String csvFilePath){
+        this.colonne_type = new ArrayList<String>() ;
+        this.colonne_name = new ArrayList<String>() ;
+        
+        File file = new File(csvFilePath);
         BufferedReader br;
+
+        
+        
         try {
             br = new BufferedReader(new FileReader(file));
             
@@ -63,13 +61,18 @@ public class Dataframe {
             Collections.addAll(colonne_type,line);
             
             line = br.readLine().split(",");
+
+            int n_l = colonne_size(csvFilePath) ;
+            int n_c = line.length ;
+            dataframe = new Object[n_l][n_c] ;
+
             Collections.addAll(colonne_name,line);
             String tmps;
             int i = 0;
             while((tmps=br.readLine())!=null){
                 line = tmps.split(",");
                 for (int j = 0; j < line.length; j++) {
-                    dataframe[i][j]=line[j];
+                    dataframe[i][j] = line[j];
                 }
                 i++;
             }
@@ -77,6 +80,27 @@ public class Dataframe {
             e.printStackTrace();
         }
         
+    }
+
+
+    public static int colonne_size(String fileName) {
+
+        Path path = Paths.get(fileName);
+  
+        int lines = 0;
+        try {
+  
+            // much slower, this task better with sequence access
+            //lines = Files.lines(path).parallel().count();
+  
+            lines = (int) Files.lines(path).count();
+  
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+  
+        return lines;
+  
     }
     
 }
---
+++ myapp/src/test/java/com/mycompany/myapp/DataframeTest.java
@@ -0,0 +1,121 @@
+package com.mycompany.myapp;
+
+import java.util.ArrayList;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class DataframeTest extends TestCase{
+     /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    
+    public DataframeTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite();
+
+        suite.addTest(new DataframeTest("Constructor1Test"));
+        suite.addTest(new DataframeTest("Constructor2Test"));
+        return suite;
+    }
+
+    public void Constructor1Test(){
+        Object[][] dataframe = new Object[2][2] ;
+        ArrayList<String> colonne_name = new ArrayList<String>();
+        ArrayList<String> colonne_type = new ArrayList<String>() ;
+
+        int n_c = 2;
+        int n_l = 2; 
+
+        for(int i=0; i<n_l; i++){
+            for(int j=0; j<n_c; j++){
+                dataframe[j][i] = (Object)i ;
+            }
+        }
+
+        colonne_name.add("A");
+        colonne_name.add("B");
+
+        colonne_type.add("java.lang.Integer");  
+        colonne_type.add("java.lang.Integer"); 
+        
+        boolean diff = false;
+        ArrayList<Object> elements = new ArrayList<Object>() ;
+        for (int i = 0; i < n_l; i++) {
+            for (int j = 0; j < n_c; j++) {
+                elements.add( dataframe[j][i] ) ;
+            }
+        }
+
+        Dataframe df = new Dataframe(colonne_name, elements) ;
+        
+        for (int j2 = 0; j2 < n_c ; j2++) {
+            
+            if(!(colonne_name.get(j2).equals(df.getColonne_name().get(j2))) || !(colonne_type.get(j2).equals(df.getColonne_type().get(j2))) ){
+                diff = true ;
+                break ;
+            }
+            
+            for (int j = 0; j < n_l; j++) {
+                if(!(dataframe[j][j2].equals(df.getDataframe()[j][j2])) || diff){
+                    diff = true ;
+                    break ;
+                } 
+            }
+        }       
+        assertTrue( !diff);
+    }
+
+    public void Constructor2Test(){
+        Object[][] dataframe = new Object[2][2] ;
+        ArrayList<String> colonne_name = new ArrayList<String>();
+        ArrayList<String> colonne_type = new ArrayList<String>() ;
+
+        int n_c = 2;
+        int n_l = 2; 
+
+        for(int i=0; i<n_l; i++){
+            for(int j=0; j<n_c; j++){
+                dataframe[j][i] = Integer.toString(i) ;
+            }
+        }
+        colonne_name.add("A");
+        colonne_name.add("B");
+
+        colonne_type.add("java.lang.Integer");  
+        colonne_type.add("java.lang.Integer"); 
+        
+        Dataframe df = new Dataframe("file.csv") ;
+        
+        boolean diff = false ;
+        for (int j2 = 0; j2 < n_c && !diff; j2++) {
+            
+            if(!(colonne_name.get(j2).equals(df.getColonne_name().get(j2))) || !(colonne_type.get(j2).equals(df.getColonne_type().get(j2))) ){
+                diff = true ;
+                break ;
+            }
+            
+            for (int j = 0; j < n_l; j++) {
+                if(!(dataframe[j][j2].equals((String)df.getDataframe()[j][j2])) || diff){
+                    diff = true ;
+                    System.out.println("my dataframe :" + dataframe[j][j2]+" construct df "+ (String)df.getDataframe()[j][j2]);
+                    break ;
+                } 
+            }
+            
+        }
+        
+        assertTrue( !diff);
+    }
+}
